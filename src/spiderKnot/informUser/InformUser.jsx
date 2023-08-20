57b45d2a0658c08cdd6ce8878bafa30cde9e914c
Esat-Karakaya <esat1karakaya@gmail.com>
2023-08-20 15:23:08 +0300

    update website

--- src/spiderKnot/informUser/InformUser.jsx
+++ src/spiderKnot/informUser/InformUser.jsx
@@ -16,15 +16,12 @@ const InformUser = ({onRouteChange}) => {
         <div className="form informUsers">
             <h1 style={{fontSize: "2em", textAlign: "center"}}>Face Detector</h1>
             <h2 style={marginLeft}>About</h2>
-            <p className="formP">This is a website that shows faces in a foto.
-             It has a database and it talks with clarifai-api and face-detector-backend</p>
+            <p className="formP">This is a website that shows faces in a foto. It talks with face-detector-backend</p>
             
             <h2 style={marginLeft}>Security Recommendation</h2>
             <p className="formP">When you register DON'T use your real name, DON'T use your real email, and I DON'T recomend 
             using a password that you already used on a different site. 
-            Altough your password is protected by hash. Your name and email is not. 
-            So for those use something silly like: 
-            "Unnamed Person" and "i_love_patetos@burger.tomato"</p>
+            </p>
 
             <h2 style={marginLeft}>Additional Content</h2>
             <p className="formP">You can visit my github account from <a href="https://github.com/Murat-Karakaya" target="_blank">here</a> 
